3b564662ae7a19cd9786e750b0ac5f62d2b4184c
Sebastien Fremy <sebastien.fremy1986@gmail.com>
2021-06-13 09:31:05 +0200

    Code du TP13

--- src/main/java/com/mycompany/dvdstore/core/App.java
+++ src/main/java/com/mycompany/dvdstore/core/App.java
@@ -2,17 +2,24 @@ package com.mycompany.dvdstore.core;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
 import com.mycompany.dvdstore.core.controller.MovieController;
 
 /**
  * Hello world!
  *
  */
+@Configuration
+@ComponentScan(basePackages = {"com.mycompany.dvdstore.core.controller", "com.mycompany.dvdstore.core.service", "com.mycompany.dvdstore.core.repository.file"})
+@PropertySource("classpath:application.properties")
 public class App 
 {
     public static void main( String[] args )
     {
-    	ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+    	ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
 
     	MovieController movieController = context.getBean(MovieController.class);
     	
